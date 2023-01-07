0a90afbd86da6c5cba1e90bf17cd7051184e02db
Alexey492 <121538373+Alexey492@users.noreply.github.com>
2023-01-07 20:43:43 +0500

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,38 +1,28 @@
-public class MainClass {
-    // ошибка - satietyTime (сытность) всегда уходит в минус и не восстанавливается. Почему?
+public class Main { // ошибка - satietyTime (сытность) всегда уходит в минус и не восстанавливается. Почему?
 
-        static int Time = 0; // вопрос - почему Time только static? иначе выдаёт ошибку
+    static int Time = 1; // вопрос - почему Time только static? иначе выдаёт ошибку
 
-        public static void main(String[] args) {
-            Cat[] cat = new Cat[3];
-            cat[0] = new Cat("Max", 15, 3);
-            cat[1] = new Cat("Barsik", 10, 1);
-            cat[2] = new Cat("Boris", 5, 2);
+    public static void main(String[] args) {
+        Cat[] cat = new Cat[3];
+        cat[0] = new Cat("Max", 15);
+        cat[1] = new Cat("Barsik", 10);
+        cat[2] = new Cat("Boris", 5);
 
-            Plate plate = new Plate(100);
+        Plate plate = new Plate(15); // как сделать так, чтобы массив котов ел каждый из своей миски? Map мы ещё не проходили в тот момент
 
-            System.out.println("У нас есть 3 кота: " + cat[0].name + ", " + cat[1].name + ", " + cat[2].name +
-                    "\n Они все хотят жрать. У каждого свой аппетит и длительность насыщения");
+        System.out.println("У нас есть 3 кота: " + cat[0].name + ", " + cat[1].name + ", " + cat[2].name +
+                "\n Они все хотят жрать, у каждого есть свой аппетит");
 
-            do {
-                for (Cat o : cat) {
-                    if (o.isSatiety() == false || o.satietyTime <= 0) {
-
-                        if (!plate.checkFood(o.getAppetite())) { // почему вместо ! нельзя написать == false? выдаёт ошибку
-                            plate.increaseFood();
-                        }
-                        o.eat(plate);
-                        System.out.println("Кот " + o.getName() + " съел " + o.getAppetite() + " граммов корма и " +
-                                "проголодается через " + (o.getSatietyTime()) + " часа(ов)");
+            for (Cat o : cat) {
+                if (o.satiety == false) {
+                    if (!plate.checkFood(o.getAppetite())) { // почему вместо ! нельзя написать == false? выдаёт ошибку
+                        plate.increaseFood();
                     }
-                    o.satietyTime--;
+                    o.eat(plate);
+                    System.out.println("Кот " + o.getName() + " съел " + o.getAppetite() + " граммов корма и лежит " +
+                            "сытый и довольный");
                 }
-                System.out.println("\nС момента начала кормежки прошел(ло) " + Time + " час(а). В миске осталось "
-                        + plate.getAmount() + " граммов корма.\n");
-                Time++;
-
-            } while (Time <= 4);
-        }
-
-
+            }
+    }
 }
+
